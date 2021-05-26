ddfd778fd5da5a7f0c8b064325831972e7d30129
shuyiwu520 <1647255309@qq.com>
2021-05-27 01:28:38 +0800

    学习python，提交字典类型的学习记录

---
+++ dictionaries/PythonDictionaryStruct.py
@@ -0,0 +1,7 @@
+# 字典的每个键值 key=>value 对用冒号 : 分割，
+# 每个对之间用逗号(,)分割，整个字典包括在花括号 {} 中 ,格式如下所示：
+dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
+
+print("dict['Name']: ", dict['Name'])
+print("dict['Age']: ", dict['Age'])
+print(dict['Class'])
